c2313d19ba9dc0150386abd957d7c92784e89f4b
Christopher Miles <mail@christophermiles.co.uk>
2018-04-27 22:28:26 -0400

    Added getters for store properties

--- src/store.js
+++ src/store.js
@@ -6,11 +6,39 @@ Vue.use(Vuex);
 export const store = new Vuex.Store({
     state: {
         registrations: [],
-        users: [
-            {id: 1, name: 'Max', registered: false},
-            {id: 2, name: 'Anna', registered: false},
-            {id: 3, name: 'Chris', registered: false},
-            {id: 4, name: 'Sven', registered: false}
+        users: [{
+                id: 1,
+                name: 'Max',
+                registered: false
+            },
+            {
+                id: 2,
+                name: 'Anna',
+                registered: false
+            },
+            {
+                id: 3,
+                name: 'Chris',
+                registered: false
+            },
+            {
+                id: 4,
+                name: 'Sven',
+                registered: false
+            }
         ]
+    },
+    getters: {
+        unregisteredUsers(state) {
+            return state.users.filter(user => {
+                return !user.registered;
+            });
+        },
+        registrations(state) {
+            return state.registrations;
+        },
+        totalRegistrations(state) {
+            return state.registrations.length;
+        }
     }
 });
